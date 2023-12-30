5246c322c4381fded69b6ae9f4cf1f3bec2781b8
Yousef <yousefsameh24@gmail.com>
2023-12-30 20:46:42 +0200

    Update schema.js with new resolvers and logger

--- backend/schema.js
+++ backend/schema.js
@@ -1,5 +1,5 @@
 const { makeExecutableSchema } = require('@graphql-tools/schema')
-const merge = require('lodash')
+const merge = require('lodash/merge')
 const {
     typeDefs: User,
     resolvers: userResolvers
@@ -9,17 +9,23 @@ const Query = `
   type Query {
     test: String
   }
+
+  type Mutation {
+    _empty: String
+  }
 `;
 
 const resolvers = {
-  Query: { 
-    test: () => 'Test success!'
-  }
+    Query: {
+        test: () => 'Hello World'
+    },
+    Mutation: {}
 };
 
 const schema = makeExecutableSchema({
     typeDefs: [Query, User],
-    resolvers: merge(resolvers, userResolvers)
+    resolvers: merge(resolvers, userResolvers),
+    logger: { log: e => console.log(e) }
 });
 
 module.exports = schema
