51694b3dff1da8ffd22137db772eb7b060bc69d5
Yousef <yousefsameh24@gmail.com>
2023-12-30 19:00:51 +0200

    Add name field to user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
     username: {
         type: String,
         required: true,
