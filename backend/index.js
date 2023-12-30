5ad2ad4de3b50fb86880518369d398d7dfb21892
Yousef <yousefsameh24@gmail.com>
2023-12-30 18:32:26 +0200

    Add initial backend index.js file

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const { ApolloServer } = require('@apollo/server')
+const { startStandaloneServer } = require('@apollo/server/standalone')
+const { GraphQLError } = require('graphql')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
+const config = require('./utils/config')
+
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen({ 
+    listen: { port: config.PORT },
+}).then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+})
