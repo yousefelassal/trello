a7d4450e6ab6538b53f8d5fe03bb53ef511de5c2
Yousef <yousefsameh24@gmail.com>
2023-12-31 15:41:05 +0200

    Add Bento component to LandingPage

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,6 +1,7 @@
 import Spotlight from "@/components/ui/spotlight";
 import TypewriterEffectSmooth from "@/components/ui/typewriter-effect";
 import { ChevronRight } from "lucide-react";
+import Bento from "@/components/Bento";
 
 export default function LandingPage() {
   const words = [
@@ -17,11 +18,12 @@ export default function LandingPage() {
 
   return (
     <>
-    <div className="h-[40rem] md:min-h-screen w-full rounded-md flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-[40rem] md:min-h-screen w-full rounded-md flex items-center justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
         className="top-10 left-0 md:left-60 md:-top-20"
         fill="white"
       />
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <div className="flex items-center flex-col">
           <h1 className="text-6xl md:text-8xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
@@ -43,7 +45,11 @@ export default function LandingPage() {
         </div>
       </div>
     </div>
-    <div className="h-screen"></div>
+    <div className="p-4 md:px-0 bg-black bg-grid-small-white/[0.2] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <Bento />
+    </div>
+    <div className="h-20"></div>
     </>
   );
 }
