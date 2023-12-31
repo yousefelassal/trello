43b4393690d585bd60048abe2e7d1f135874aa44
Yousef <yousefsameh24@gmail.com>
2023-12-31 13:31:37 +0200

    Refactor App component and add floating navigation bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
 import { useApolloClient } from "@apollo/client"
-import LandingPage from "./components/LandingPage"
-import Home from "./components/Home"
-import Signup from "./components/Signup"
-import Login from "./components/Login"
 import {
   Routes,
   Route,
 } from "react-router-dom"
+
+import { Code2, UserRoundPlus } from "lucide-react"
+
+import LandingPage from "@/components/LandingPage"
+import FloatingNav from "@/components/ui/floating-navbar"
+import Home from "@/components/Home"
+import Signup from "@/components/Signup"
+import Login from "@/components/Login"
 
 export default function App() {
   const [token, setToken] = useState<string | null>(null)
@@ -23,7 +27,18 @@ export default function App() {
     <>
       <Routes>
         <Route path="/" element={
-          token ? <Home logout={logout} /> : <LandingPage />
+          token ? 
+          <Home logout={logout} /> 
+          : 
+          <>
+            <FloatingNav 
+              navItems={[
+                { name: "Docs", link: "/documentation", icon: <Code2 /> },
+                { name: "Signup", link: "/signup", icon: <UserRoundPlus /> },
+              ]}
+            />
+            <LandingPage />
+          </>
         } />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login setToken={setToken} />} />
