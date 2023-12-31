3b11f82bd80f822737477e52f4562f760d6b4214
Yousef <yousefsameh24@gmail.com>
2023-12-31 15:56:27 +0200

    Refactor Skeleton component in Bento.tsx

--- frontend/src/components/Bento.tsx
+++ frontend/src/components/Bento.tsx
@@ -23,7 +23,7 @@ export default function Bento() {
   );
 }
 const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl  bg-dot-white/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-white/[0.2] bg-black"></div>
+    <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl  bg-dot-white/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-white/[0.2] bg-black" />
 );
 const items = [
   {
