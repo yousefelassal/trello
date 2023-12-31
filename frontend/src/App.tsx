525435001d0dfbd2b23d4b438a82b9472adfc27a
Yousef <yousefsameh24@gmail.com>
2023-12-31 12:51:14 +0200

    Refactor App component and add routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import { useApolloClient } from "@apollo/client"
+import LandingPage from "./components/LandingPage"
+import Home from "./components/Home"
+import Signup from "./components/Signup"
+import Login from "./components/Login"
+import {
+  Routes,
+  Route,
+} from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [token, setToken] = useState<string | null>(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={
+          token ? <Home logout={logout} /> : <LandingPage />
+        } />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
+      </Routes>
     </>
   )
 }
-
-export default App
