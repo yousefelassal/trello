9bb2e1636c503ab0bceb869c48116eb10e9fb91b
Yousef <yousefsameh24@gmail.com>
2024-01-01 16:35:56 +0200

    Refactor ApolloProvider and add Jotai Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,39 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter as Router } from "react-router-dom"
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  createHttpLink,
-} from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('trello-3al-daya2-token')
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : null,
-    }
-  }
-})
-
-console.log(import.meta.env.VITE_SERVER)
-
-const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_SERVER,
-})
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
-})
+import ApolloProvider from './components/ApolloProvider.tsx'
+import { Provider } from 'jotai'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ApolloProvider client={client}>
-    <Router>
-      <App />
-    </Router>
-  </ApolloProvider>
+  <Provider>
+    <ApolloProvider>
+      <Router>
+        <App />
+      </Router>
+    </ApolloProvider>
+  </Provider>
 )
