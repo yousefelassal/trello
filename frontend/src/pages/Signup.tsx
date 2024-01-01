01f4d33322db6cafc00f892956e6bf6c0302389a
Yousef <yousefsameh24@gmail.com>
2024-01-01 16:37:55 +0200

    Refactor Signup component to remove unused code

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,25 +1,19 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { useMutation } from "@apollo/client"
 import { LOGIN, SIGNUP } from "../graphql/mutations"
-import { useNavigate } from "react-router-dom"
 
 const Signup = ({ setToken }:{ setToken:(value:string) => void }) => {
     const [name, setName] = useState('')
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const navigate = useNavigate()
     
     const [signup] = useMutation(SIGNUP)
-    const [login, result] = useMutation(LOGIN)
-
-    useEffect(() => {
-        if (result.data) {
-            const token = result.data.login.value
+    const [login] = useMutation(LOGIN, {
+        onCompleted(data) {
+            const token = data.login.value
             setToken(token)
-            localStorage.setItem('trello-3al-daya2-token', token)
-            navigate('/')
-        }
-    }, [result.data]) // eslint-disable-line
+        },
+    })
 
     const submit = async (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
