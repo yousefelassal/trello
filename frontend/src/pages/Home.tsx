b17b21e73cceb54901f7f92a94b75c6d646bb833
Yousef <yousefsameh24@gmail.com>
2024-01-01 17:05:27 +0200

    Add background overlay and container to Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,9 +9,14 @@ export default function Home({logout}: {logout: () => void}) {
   if (loading) return <div>Loading...</div>
 
   return (
-    <div>
-        <h1 className="text-4xl font-bold">Welcome {data.me.name}</h1>
-        <button onClick={logout}>Logout</button>
+    <>
+    <div className="fixed -z-10 h-screen w-full bg-black bg-dot-white/[0.2] flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
     </div>
+    <div className="container">
+      <h1 className="text-4xl font-bold">Welcome {data.me.name}</h1>
+      <button onClick={logout}>Logout</button>
+    </div>
+    </>
   )
 }
