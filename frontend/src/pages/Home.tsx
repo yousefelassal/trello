830918e2bbcceb9d7f64d2d19de9f14cc4979fe1
Yousef <yousefsameh24@gmail.com>
2024-01-02 15:07:48 +0200

    Refactor Home component to display user boards and add new board

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,23 @@
 import { useQuery } from '@apollo/client'
-import { MeDocument, MeQuery, MeQueryVariables } from '@/generated/graphql'
+import {
+  MeDocument,
+  MeQuery,
+  MeQueryVariables,
+  AllBoardsDocument,
+  AllBoardsQuery,
+  AllBoardsQueryVariables,
+} from '@/generated/graphql'
 import Board from '@/components/Board'
+import AddBoard from '@/components/AddBoard'
 
 export default function Home({logout}: {logout: () => void}) {
-  const { data, loading, error } = useQuery<MeQuery, MeQueryVariables>(MeDocument)
+  const { data:user, loading: loadingUser, error: errorUser } = useQuery<MeQuery, MeQueryVariables>(MeDocument)
 
-  if (error) return <div>{error.message}</div>
+  const { data:boards, loading:boardsLoading, error:errorBoards } = useQuery<AllBoardsQuery, AllBoardsQueryVariables>(AllBoardsDocument)
+
+  if (errorUser) return <div>{errorUser.message}</div>
   
-  if (loading) return <div>Loading...</div>
+  if (loadingUser) return <div>Loading...</div>
 
   return (
     <>
@@ -16,19 +26,26 @@ export default function Home({logout}: {logout: () => void}) {
     </div>
     <div className="container flex flex-col gap-12">
       <div className="flex justify-between">
-        <h1 className="text-4xl font-bold">Welcome {data?.me?.name}</h1>
+        <h1 className="text-4xl font-bold">Welcome {user?.me?.name}</h1>
         <button onClick={logout}>Logout</button>
       </div>
-      <div className="grid md:auto-rows-[12rem] grid-cols-1 md:grid-cols-5 gap-4 max-w-7xl">
-        {data?.me?.boards.map((board) => (
-          <Board
+      <div className="flex flex-col gap-2">
+        <h2 className="text-2xl font-bold">Your Boards</h2>
+        {boards?.allBoards?.length === 0 && <div>You don't have any boards yet</div>}
+        <div className="grid md:auto-rows-[12rem] grid-cols-1 md:grid-cols-5 gap-4 max-w-7xl">
+          {boardsLoading && <div>Loading...</div>}
+          {errorBoards && <div>{errorBoards.message}</div>}
+          {boards?.allBoards?.map((board) => (
+            <Board
             key={board.id}
             to={`/${board.id}`}
             title={board.title}
             bg={board.bg}
-          />
-        ))}
+            />
+            ))}
+        </div>
       </div>
+      <AddBoard />
     </div>
     </>
   )
