069afa8868a159f77c73f1ea53628d052cc22cc5
Yousef <yousefsameh24@gmail.com>
2024-01-02 17:45:47 +0200

    Add FRONTEND_URL to config.js

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -5,7 +5,12 @@ const MONGODB_URI = process.env.NODE_ENV === 'development'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
+const FRONTEND_URL = process.env.NODE_ENV === 'development'
+  ? process.env.DEV_FRONTEND_URL
+  : process.env.PROD_FRONTEND_URL
+
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  FRONTEND_URL
 }
