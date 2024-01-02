feb4c7d8f4da95eb409aca380a5ba38ebf07fa2a
Yousef <yousefsameh24@gmail.com>
2024-01-02 12:32:10 +0200

    Add Board component to Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/client'
 import { MeDocument, MeQuery, MeQueryVariables } from '@/generated/graphql'
+import Board from '@/components/Board'
 
 export default function Home({logout}: {logout: () => void}) {
   const { data, loading, error } = useQuery<MeQuery, MeQueryVariables>(MeDocument)
@@ -13,16 +14,19 @@ export default function Home({logout}: {logout: () => void}) {
     <div className="fixed -z-10 h-screen w-full bg-black bg-dot-white/[0.2] flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
     </div>
-    <div className="container flex flex-col">
+    <div className="container flex flex-col gap-12">
       <div className="flex justify-between">
         <h1 className="text-4xl font-bold">Welcome {data?.me?.name}</h1>
         <button onClick={logout}>Logout</button>
       </div>
-      <div className="flex">
+      <div className="grid md:auto-rows-[12rem] grid-cols-1 md:grid-cols-5 gap-4 max-w-7xl">
         {data?.me?.boards.map((board) => (
-          <div className="flex flex-col" key={board.id}>
-            <h2 className="text-2xl font-bold">{board.title}</h2>
-          </div>
+          <Board
+            key={board.id}
+            to={`/${board.id}`}
+            title={board.title}
+            bg={board.bg}
+          />
         ))}
       </div>
     </div>
