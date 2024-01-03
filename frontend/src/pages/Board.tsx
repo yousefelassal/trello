bff57cacec65021bb65f50dd34dfa505ce95b334
Yousef <yousefsameh24@gmail.com>
2024-01-03 14:55:48 +0200

    Refactor Board component to display lists and cards

--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -19,7 +19,24 @@ export default function Board() {
             <img src={data?.findBoard?.bg} alt="board background" className="object-cover absolute inset-0" />
         </div>
         <div className="container flex flex-col">
-            Board
+            <div className="flex flex-col gap-2">
+                <h2 className="text-2xl font-bold">{data?.findBoard?.title}</h2>
+                <div className="grid md:auto-rows grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl">
+                    {data?.findBoard?.lists?.map((list) => (
+                        <div key={list.id} className="flex flex-col gap-2">
+                            <h3 className="text-xl font-bold">{list.title}</h3>
+                            <div className="flex flex-col gap-2">
+                                {list.cards?.map((card) => (
+                                    <div key={card.id} className="flex flex-col gap-2">
+                                        <h4 className="text-lg font-bold">{card.title}</h4>
+                                        <div>{card.description}</div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     </>
   )
