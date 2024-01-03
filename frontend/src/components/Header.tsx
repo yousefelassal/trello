ada75cb34b0dee16db4c17ef29b5f8f547ee12f9
Yousef <yousefsameh24@gmail.com>
2024-01-03 18:14:25 +0200

    Update Header component styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header({logout}: {logout: () => void}) {
 
   if (loading) return <div>Loading...</div>
   return (
-    <div className="container fixed inset-x-0 py-2 backdrop-blur flex justify-between border-b border-neutral-600 bg-[hsla(0,0%,100%,.1)]">
+    <div className="container fixed inset-x-0 py-2 backdrop-blur z-[9999] flex justify-between border-b border-neutral-600 bg-[hsla(0,0%,100%,.1)]">
       <div className="flex gap-2 items-center">
         <Link to="/" className="text-2xl font-bold">Home</Link>
         <h1 className="text-2xl font-bold">Welcome {user?.me?.name}</h1>
