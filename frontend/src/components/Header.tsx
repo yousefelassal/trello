c95801ffdb508b043d53bb0368f35abb8f5998bb
Yousef <yousefsameh24@gmail.com>
2024-01-03 12:49:43 +0200

    Add link to home page in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,6 +4,7 @@ import {
   MeQuery,
   MeQueryVariables,
 } from '@/generated/graphql'
+import { Link } from 'react-router-dom'
 
 export default function Header({logout}: {logout: () => void}) {
   const { data:user, loading, error } = useQuery<MeQuery, MeQueryVariables>(MeDocument)
@@ -13,7 +14,10 @@ export default function Header({logout}: {logout: () => void}) {
   if (loading) return <div>Loading...</div>
   return (
     <div className="container py-2 backdrop-blur flex justify-between border-b border-neutral-600 bg-[hsla(0,0%,100%,.1)]">
-      <h1 className="text-4xl font-bold">Welcome {user?.me?.name}</h1>
+      <div className="flex gap-2 items-center">
+        <Link to="/" className="text-2xl font-bold">Home</Link>
+        <h1 className="text-2xl font-bold">Welcome {user?.me?.name}</h1>
+      </div>
       <button onClick={logout}>Logout</button>
     </div>
   )
