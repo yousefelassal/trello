ed9a3f4e9c2ed5ee62804a2b70c28fca100c2a46
Yousef <yousefsameh24@gmail.com>
2024-01-04 16:37:12 +0200

    Update Board component styles

--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -87,12 +87,12 @@ export default function Board() {
         <div className="-z-10 fixed inset-0 overflow-hidden">
             <img src={data?.findBoard?.bg} alt="board background" className="object-cover absolute w-screen h-screen inset-0" />
         </div>
-        <div className="fixed px-6 py-2 w-screen shadow -mt-4 bg-black/60">
+        <div className="fixed px-6 backdrop-blur-sm py-2 w-screen shadow -mt-4 bg-black/60">
             <h2 className="text-2xl w-fit p-2 py-1 hover:bg-gray-700 rounded-lg font-bold">{data?.findBoard?.title}</h2>
         </div>
         <div className="container pt-16 flex gap-2">
             {data?.findBoard?.lists?.map((list) => (
-                <div key={list.id} className="flex p-2 flex-col min-w-[272px] rounded-2xl shadow-2xl border border-[#201f1f] bg-[#101204] gap-2">
+                <div key={list.id} className="flex p-2 flex-col min-w-[272px] h-fit rounded-2xl shadow-2xl border border-[#201f1f] bg-[#101204] gap-2">
                     <h3 className="text-xl font-bold">{list.title}</h3>
                     <div className="flex flex-col gap-2">
                         {list.cards?.map((card) => (
