cbca835f1df83c752bf941a1708c35c4ab4410f8
Yousef <yousefsameh24@gmail.com>
2024-01-04 13:03:43 +0200

    Remove login header

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -29,7 +29,6 @@ const Login = ({ setToken }:{ setToken:any }) => {
     return (
         <div className="min-h-screen w-full rounded-md flex items-center justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-        <h2>Login</h2>
         <form onSubmit={submit}>
             <div>
             username <input
