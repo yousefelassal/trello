6ca1cf88c82159af7f0b17c2196b12eac43f1e65
Yousef <yousefsameh24@gmail.com>
2024-01-04 12:45:32 +0200

    Update Signup page styling

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -33,8 +33,8 @@ const Signup = ({ setToken }:{ setToken: any }) => {
     }
     
     return (
-        <div>
-        <h2>Signup</h2>
+        <div className="min-h-screen w-full rounded-md flex items-center justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         <form onSubmit={submit}>
             <div>
             name <input
