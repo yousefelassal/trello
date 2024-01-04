82b55f75a4f2788951f2747703d890e8c4802f0e
Yousef <yousefsameh24@gmail.com>
2024-01-04 12:45:10 +0200

    Update Docs page styling

--- frontend/src/pages/Docs.tsx
+++ frontend/src/pages/Docs.tsx
@@ -1,6 +1,9 @@
 
 export default function Docs() {
   return (
-    <div>Docs</div>
+    <div className="min-h-screen w-full rounded-md flex items-center justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        Docs
+    </div>
   )
 }
