d1a5146c87e191ab011422e8a19247dc07093f34
Yousef <yousefsameh24@gmail.com>
2024-01-04 13:03:31 +0200

    Update import statement for Toaster component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom"
 import ApolloProvider from './components/ApolloProvider.tsx'
 import { Provider } from 'jotai'
 import { Provider as TextProvider } from 'react-wrap-balancer'
-import { Toaster } from '@/components/ui/sonner'
+import { Toaster } from 'sonner'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider>
