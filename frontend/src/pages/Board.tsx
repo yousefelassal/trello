f657828ce9af8ca034a1fda2669b965213098896
Yousef <yousefsameh24@gmail.com>
2024-01-04 17:32:43 +0200

    Add drag and drop functionality to Board component

--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -10,7 +10,11 @@ import {
     AddCardDocument,
     AddCardMutation,
     AddCardMutationVariables,
+    UpdateBoardDocument,
+    UpdateBoardMutation,
+    UpdateBoardMutationVariables
 } from "@/generated/graphql"
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 export default function Board() {
   const { id } = useParams()
@@ -42,6 +46,8 @@ export default function Board() {
   })
 
   const [addCard] = useMutation<AddCardMutation, AddCardMutationVariables>(AddCardDocument)
+
+  const [updateBoard] = useMutation<UpdateBoardMutation, UpdateBoardMutationVariables>(UpdateBoardDocument)
 
   if (error) return <div>{error.message}</div>
 
@@ -82,6 +88,95 @@ export default function Board() {
     })
   }
 
+    const onDragEnd = async (result:any) => { //eslint-disable-line
+        const { destination, source, draggableId, type } = result;
+    
+        if(!destination) return;
+    
+        if(
+            destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) return;
+    
+        if(type === "column") {
+            const newLists = Array.from(data?.findBoard?.lists as any[]) //eslint-disable-line
+            newLists.splice(source.index, 1)
+            newLists.splice(destination.index, 0, data?.findBoard?.lists?.find((list) => list.id === draggableId) as any) //eslint-disable-line
+            updateBoard({
+                variables: {
+                    updateBoardId: id as string,
+                    title: data?.findBoard?.title as string,
+                    bg: data?.findBoard?.bg as string,
+                    description: data?.findBoard?.description as string,
+                    lists: newLists,
+                    updated_at: new Date().toISOString()
+                }
+            })
+            return
+        }
+    
+        const start = data?.findBoard?.lists?.find((list) => list.id === source.droppableId) as any //eslint-disable-line
+        const finish = data?.findBoard?.lists?.find((list) => list.id === destination.droppableId) as any //eslint-disable-line
+    
+        if(start === finish) {
+            const newCards = Array.from(start.cards as any[]) //eslint-disable-line
+            newCards.splice(source.index, 1)
+            newCards.splice(destination.index, 0, start.cards?.find((card:any) => card.id === draggableId) as any) //eslint-disable-line
+            updateBoard({
+                variables: {
+                    updateBoardId: id as string,
+                    title: data?.findBoard?.title as string,
+                    bg: data?.findBoard?.bg as string,
+                    updated_at: new Date().toISOString(),
+                    description: data?.findBoard?.description as string,
+                    lists: data?.findBoard?.lists?.map((list) => {
+                        if(list.id === source.droppableId) {
+                            return {
+                                ...list,
+                                cards: newCards
+                            }
+                        }
+                        return list
+                    }) as any[] //eslint-disable-line
+                }
+            })
+            return
+        }
+    
+        const startCards = Array.from(start.cards as any[]) //eslint-disable-line
+        startCards.splice(source.index, 1)
+        const newStart = {
+            ...start,
+            cards: startCards
+        }
+
+        const finishCards = Array.from(finish.cards as any[]) //eslint-disable-line
+        finishCards.splice(destination.index, 0, start.cards?.find((card:any) => card.id === draggableId) as any) //eslint-disable-line
+        const newFinish = {
+            ...finish,
+            cards: finishCards
+        }
+
+        updateBoard({
+            variables: {
+                updateBoardId: id as string,
+                title: data?.findBoard?.title as string,
+                bg: data?.findBoard?.bg as string,
+                updated_at: new Date().toISOString(),
+                description: data?.findBoard?.description as string,
+                lists: data?.findBoard?.lists?.map((list) => {
+                    if(list.id === source.droppableId) {
+                        return newStart
+                    }
+                    if(list.id === destination.droppableId) {
+                        return newFinish
+                    }
+                    return list
+                }) as any[] //eslint-disable-line
+            }
+        })
+    }
+
   return (
     <>
         <div className="-z-10 fixed inset-0 overflow-hidden">
@@ -90,17 +185,51 @@ export default function Board() {
         <div className="fixed px-6 backdrop-blur-sm py-2 w-screen shadow -mt-4 bg-black/60">
             <h2 className="text-2xl w-fit p-2 py-1 hover:bg-gray-700 rounded-lg font-bold">{data?.findBoard?.title}</h2>
         </div>
-        <div className="container pt-16 flex gap-2">
-            {data?.findBoard?.lists?.map((list) => (
-                <div key={list.id} className="flex p-2 flex-col min-w-[272px] h-fit rounded-2xl shadow-2xl border border-[#201f1f] bg-[#101204] gap-2">
-                    <h3 className="text-xl font-bold">{list.title}</h3>
-                    <div className="flex flex-col gap-2">
-                        {list.cards?.map((card) => (
-                            <div key={card.id} className="flex bg-[#22272B] rounded-lg justify-center px-2 py-1 shadow-2xl flex-col">
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="all-columns" direction="horizontal" type="column">
+        {(provided:any) => ( //eslint-disable-line
+            <div 
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              className="container pt-16 flex gap-2">
+            {data?.findBoard?.lists?.map((list, index) => (
+                <Draggable draggableId={list.id} index={index} key={list.id}>
+                {(provided:any) => ( //eslint-disable-line
+                <div 
+                  {...provided.draggableProps}
+                  ref={provided.innerRef}
+                  className="flex p-2 flex-col min-w-[272px] h-fit rounded-2xl shadow-2xl border border-[#201f1f] bg-[#101204] gap-2"
+                >
+                    <h3
+                        className="text-xl w-full font-bold"
+                        {...provided.dragHandleProps}
+                    >
+                        {list.title}
+                    </h3>
+                    <Droppable droppableId={list.id} type="task">
+                    {(provided:any, snapshot:any) => ( //eslint-disable-line
+                    <div 
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={`flex flex-col gap-2 transition ${snapshot.isDraggingOver ? 'bg-blue-400' : ''}`}
+                    >
+                        {list.cards?.map((card, index) => (
+                            <Draggable draggableId={card.id} index={index} key={card.id}>
+                            {(provided:any, snapshot:any) => ( //eslint-disable-line
+                                <div 
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  className={`${snapshot.isDragging ? 'bg-slate-400' : 'bg-gray-200' }
+                                  flex bg-[#22272B] rounded-lg justify-center px-2 py-1 shadow-2xl flex-col`}
+                                >
                                 <h4 className="text-lg font-bold">{card.title}</h4>
                                 <div>{card.description}</div>
                             </div>
+                            )}
+                            </Draggable>
                         ))}
+                        {provided.placeholder}
                         <button
                             onClick={() => addNewCard(list)}
                             className="bg-[#ffffff3d] hover:bg-[#ffffff2a] transition rounded-lg h-fit p-2 flex items-center shadow-lg"
@@ -108,15 +237,23 @@ export default function Board() {
                             {list.cards?.length === 0 ? "Add a card" : "Add another card"}
                         </button>
                     </div>
+                    )}
+                    </Droppable>
                 </div>
+                )}
+                </Draggable>
             ))}
+            {provided.placeholder}
             <button
-                onClick={addNewList}
-                className="bg-[#ffffff3d] hover:bg-[#ffffff2a] transition rounded-lg min-w-[272px] h-fit p-2 flex items-center shadow-lg"
+            onClick={addNewList}
+            className="bg-[#ffffff3d] hover:bg-[#ffffff2a] transition rounded-lg min-w-[272px] h-fit p-2 flex items-center shadow-lg"
             >
                 {data?.findBoard?.lists?.length === 0 ? "Add your first list" : "Add another list"}
             </button>
-        </div>
+            </div>
+        )}
+        </Droppable>
+      </DragDropContext>
     </>
   )
 }
