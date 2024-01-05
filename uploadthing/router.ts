416de697d81f10da67e71ec22d507cc13cb635c0
Yousef <yousefsameh24@gmail.com>
2024-01-05 18:01:41 +0200

    Refactor image upload middleware and add console log for upload completion

--- uploadthing/router.ts
+++ uploadthing/router.ts
@@ -12,30 +12,41 @@ const f = createUploadthing({
   },
 });
 
+// export const uploadRouter = {
+//   image: f({
+//     image: {
+//       maxFileSize: "4MB",
+//       maxFileCount: 1
+//     }
+//   })
+//   .middleware(async ({ req }) => {
+//     const auth = req ? req.headers.get('authorization') : null
+//     if(auth && auth.toLowerCase().startsWith('bearer ')) {
+//       const decodedToken = jwt.verify(
+//         auth.substring(7), process.env.JWT_SECRET as string
+//       ) as JwtPayload;
+//       const currentUser = await User.findById(decodedToken.id)
+//       if(!currentUser) {
+//         throw new Error('token missing or invalid')
+//       }
+//       return { currentUser }
+//     }
+//     return {}; // Add this line to return an empty object
+//   })
+//   .onUploadComplete(async ({ metadata }) => {
+//     console.log("Uploaded by user", metadata.currentUser?.name);
+//   }),
+// } satisfies FileRouter;
+
 export const uploadRouter = {
   image: f({
     image: {
-      maxFileSize: "4MB",
-      maxFileCount: 1
+        maxFileSize: "4MB",
+        maxFileCount: 1
     }
-  })
-  .middleware(async ({ req }) => {
-    const auth = req ? req.headers.get('authorization') : null
-    if(auth && auth.toLowerCase().startsWith('bearer ')) {
-      const decodedToken = jwt.verify(
-        auth.substring(7), process.env.JWT_SECRET as string
-      ) as JwtPayload;
-      const currentUser = await User.findById(decodedToken.id)
-      if(!currentUser) {
-        throw new Error('token missing or invalid')
-      }
-      return { currentUser }
-    }
-    return {}; // Add this line to return an empty object
-  })
-  .onUploadComplete(async ({ metadata }) => {
-    console.log("Uploaded by user", metadata.currentUser?.name);
-  }),
+  }).onUploadComplete((data) => {
+    console.log("upload completed", data);
+}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof uploadRouter;
