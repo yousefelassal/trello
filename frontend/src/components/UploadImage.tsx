2329e0ca391fd28a59f1cb1e8237f7adac927193
Yousef <yousefsameh24@gmail.com>
2024-01-05 18:01:17 +0200

    Add UploadImage component

---
+++ frontend/src/components/UploadImage.tsx
@@ -0,0 +1,21 @@
+import { UploadDropzone } from "../../uploadthing";
+
+export default function UploadImage() {
+  return (
+    <div className="flex flex-col items-center gap-4 py-8">
+      <div>
+        <UploadDropzone
+          endpoint="image"
+          onClientUploadComplete={(file:any) => { //eslint-disable-line
+            console.log("uploaded", file);
+            alert("Upload complete");
+          }}
+          onUploadError={(error:any) => { //eslint-disable-line
+            console.error(error, error.cause);
+            alert("Upload failed");
+          }}
+        />
+      </div>
+    </div>
+  );
+}
