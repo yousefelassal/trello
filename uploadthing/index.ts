566f437171d8e1570bddceae9a500f08ae2ae638
Yousef <yousefsameh24@gmail.com>
2024-01-05 15:15:27 +0200

    Add MongoDB connection and error handling

--- uploadthing/index.ts
+++ uploadthing/index.ts
@@ -3,6 +3,19 @@ import "dotenv/config"
 import express from 'express';
 import { createUploadthingExpressHandler } from "uploadthing/express"
 import { uploadRouter } from './router'
+import config from './utils/config'
+import mongoose from 'mongoose'
+mongoose.set('strictQuery', false)
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI as string)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
 
 const app = express();
 app.use(cors());
