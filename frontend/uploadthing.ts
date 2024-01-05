26a3b1a3d88ad3dd969a5ebfdbbe858e64c61bfc
Yousef <yousefsameh24@gmail.com>
2024-01-05 18:01:09 +0200

    Refactor uploadthing.ts to use dynamic upload server URL

--- frontend/uploadthing.ts
+++ frontend/uploadthing.ts
@@ -1,6 +1,4 @@
 import { generateComponents } from "@uploadthing/react";
-import { generateReactHelpers } from "@uploadthing/react/hooks";
-import { createUploadthing, type FileRouter } from "uploadthing/server";
 import { useTokenValue } from './src/hooks/useTokenValue'
 
 export const useHeaders = () => {
@@ -10,29 +8,9 @@ export const useHeaders = () => {
     }
 }
 
+console.log(import.meta.env.VITE_UPLOAD_SERVER)
+
 export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
-
-const f = createUploadthing({
-      errorFormatter: (err) => {
-          console.log("Error uploading file", err.message);
-    console.log("  - Above error caused by:", err.cause);
-
-    return { message: err.message };
-  },
-});
-
-const uploadRouter = {
-  image: f({
-    image: {
-        maxFileSize: "4MB",
-        maxFileCount: 1
-    }
-  }).onUploadComplete((data) => {
-    console.log("upload completed", data);
-}),
-} satisfies FileRouter;
-
-type OurFileRouter = typeof uploadRouter;
-
-export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
+  generateComponents({
+    url: import.meta.env.VITE_UPLOAD_SERVER
+  });
