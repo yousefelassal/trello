5689c0ce2a43ab176b73fc59d054f88afe7a680b
Yousef <yousefsameh24@gmail.com>
2024-01-05 14:31:05 +0200

    Add upload router for file handling

---
+++ uploadthing/router.ts
@@ -0,0 +1,24 @@
+import { createUploadthing, type FileRouter } from "uploadthing/server";
+
+const f = createUploadthing({
+  
+  errorFormatter: (err) => {
+    console.log("Error uploading file", err.message);
+    console.log("  - Above error caused by:", err.cause);
+
+    return { message: err.message };
+  },
+});
+
+export const uploadRouter = {
+  ImageHandler: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1
+    }
+  }).onUploadComplete((data) => {
+    console.log("upload completed", data);
+  }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof uploadRouter;
