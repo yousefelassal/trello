252d15ded727be6e85a4fb84fce61226f0afe85c
Yousef <yousefsameh24@gmail.com>
2024-01-05 15:15:34 +0200

    Add authentication middleware and handle upload completion

--- uploadthing/router.ts
+++ uploadthing/router.ts
@@ -1,7 +1,9 @@
 import { createUploadthing, type FileRouter } from "uploadthing/server";
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import User from './models/user'
+import 'dotenv/config'
 
 const f = createUploadthing({
-  
   errorFormatter: (err) => {
     console.log("Error uploading file", err.message);
     console.log("  - Above error caused by:", err.cause);
@@ -16,8 +18,23 @@ export const uploadRouter = {
       maxFileSize: "4MB",
       maxFileCount: 1
     }
-  }).onUploadComplete((data) => {
-    console.log("upload completed", data);
+  })
+  .middleware(async ({ req }) => {
+    const auth = req ? req.headers.get('authorization') : null
+    if(auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET as string
+      ) as JwtPayload;
+      const currentUser = await User.findById(decodedToken.id)
+      if(!currentUser) {
+        throw new Error('token missing or invalid')
+      }
+      return { currentUser }
+    }
+    return {}; // Add this line to return an empty object
+  })
+  .onUploadComplete(async ({ metadata }) => {
+    console.log("Uploaded by user", metadata.currentUser?.name);
   }),
 } satisfies FileRouter;
 
