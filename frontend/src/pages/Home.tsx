434208776d5b26e67081482395bef015376a03bc
Yousef <yousefsameh24@gmail.com>
2024-01-06 15:30:30 +0200

    Add bottom padding to container in Home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <div className="fixed -z-10 h-screen w-full bg-black bg-dot-white/[0.2] flex items-center justify-center">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
     </div>
-    <div className="container flex flex-col gap-12">
+    <div className="container flex flex-col gap-12 pb-8">
       <div className="flex flex-col gap-2">
         <h2 className="text-2xl font-bold">Your Boards</h2>
         {boards?.allBoards?.length === 0 && <div>You don't have any boards yet</div>}
