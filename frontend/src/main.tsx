40e9c7191be44090273ed728ee72ae1c14fc3c59
Yousef <yousefsameh24@gmail.com>
2024-01-06 16:39:02 +0200

    Use HashRouter to fix 404 error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 import ApolloProvider from './components/ApolloProvider.tsx'
 import { Provider } from 'jotai'
 import { Provider as TextProvider } from 'react-wrap-balancer'
