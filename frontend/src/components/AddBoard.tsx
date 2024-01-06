5d44deac12779fdc51966e9002e6ec1cfdd5c414
Yousef <yousefsameh24@gmail.com>
2024-01-06 15:30:41 +0200

    Add dialog component for creating new boards

--- frontend/src/components/AddBoard.tsx
+++ frontend/src/components/AddBoard.tsx
@@ -1,4 +1,5 @@
 import { getRandomPhoto } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import { useState } from "react"
 import { useMutation } from "@apollo/client"
 import {
@@ -7,9 +8,29 @@ import {
     CreateBoardMutation,
     AllBoardsDocument
 } from "@/generated/graphql"
+import { useForm } from "react-hook-form"
 
-export default function AddBoard() {
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "@/components/ui/textarea"
+
+import { Plus, Loader2 } from "lucide-react"
+
+export default function AddBoard({ header, className }:{ header?:boolean, className?:string }) {
   const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const form = useForm()
     const [createBoard] = useMutation<CreateBoardMutation, CreateBoardMutationVariables>(CreateBoardDocument, {
         update: (cache, response) => {
             cache.updateQuery({ query: AllBoardsDocument }, (data) => {
@@ -23,9 +44,7 @@ export default function AddBoard() {
         }
     })
 
-    const submit = async (event:React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-    
+    const submit = async () => {
         createBoard({ variables: { 
             title, 
             bg: await getRandomPhoto()
@@ -35,15 +54,60 @@ export default function AddBoard() {
 
   return (
     <div className="flex items-center justify-center">
-        <form onSubmit={submit}>
-            <input
-            className="bg-gray-900"
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-            placeholder="Add Board"
-            />
-            <button type="submit">Add</button>
-        </form>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button className={cn('', className)}>
+                    {header ? 
+                        <Plus />
+                        : <><Plus /> <span>Create new board</span></>
+                    }
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                <DialogTitle>Add a Board</DialogTitle>
+                <DialogDescription>
+                    Add a new board to your dashboard
+                </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="title" className="sm:text-right">
+                        Title
+                    </Label>
+                    <Input
+                        id="title"
+                        className="col-span-full sm:col-span-3"
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </div>
+                <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="description" className="sm:text-right">
+                        Description
+                    </Label>
+                    <Textarea
+                        id="description"
+                        className="col-span-full sm:col-span-3"
+                        onChange={(e) => setDescription(e.target.value)}
+                        value={description}
+                    />
+                </div>
+                </div>
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button
+                            type="submit"
+                            disabled={form.formState.isSubmitting}
+                            onClick={form.handleSubmit(submit)}
+                            className="flex gap-1"
+                        >
+                            {form.formState.isSubmitting ? <> <Loader2 className="animate-spin" /> Adding...</> : 'Add'}
+                        </Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     </div>
   )
 }
