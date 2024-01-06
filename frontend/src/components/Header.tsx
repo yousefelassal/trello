17ec6160ff6c666473c131499737b611c36b1b76
Yousef <yousefsameh24@gmail.com>
2024-01-06 15:42:51 +0200

    Update Header component styling

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header({logout}: {logout: () => void}) {
   </div>
   
   return (
-    <div className="container fixed inset-x-0 py-2 backdrop-blur items-center z-[9999] flex justify-between border-b border-neutral-600 bg-[hsla(0,0%,100%,.1)]">
+    <div className="container fixed inset-x-0 py-2 backdrop-blur items-center z-40 flex justify-between border-b border-neutral-600 bg-[hsla(0,0%,100%,.1)]">
       <div className="flex gap-2">
         <Link to="/" className="group rounded-md px-2 py-1 items-center flex gap-1 transition hover:bg-gray-500/80">
           <div className="rounded-[3px] items-start p-1 h-5 w-5 flex gap-[2px] bg-[#9eacba]/80">
