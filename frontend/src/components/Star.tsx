a986c0e075665d7b722cb1c9f1939c8b07e6088f
Yousef <yousefsameh24@gmail.com>
2024-01-07 14:46:58 +0200

    Refactor Star component to include animation and additional props

--- frontend/src/components/Star.tsx
+++ frontend/src/components/Star.tsx
@@ -1,13 +1,25 @@
 import { cn } from "@/lib/utils"
 import { IconStar, IconStarFilled } from "@tabler/icons-react"
 
-export default function Star({ className, saved, onClick }:{ className?:string, saved:boolean | null | undefined, onClick?:any }) { // eslint-disable-line
+export default function Star({
+    className,
+    iconClassName,
+    saved,
+    onClick,
+    animate
+  }:{
+    className?:string,
+    iconClassName?:string,
+    saved:boolean | null | undefined,
+    onClick?:any // eslint-disable-line
+    animate?:boolean
+  }) { 
   return (
-    <button onClick={onClick} className="transition hover:scale-110">
+    <button onClick={onClick} className={cn(`transition hover:scale-110 ${animate && !saved && 'hidden group-hover:block group-hover:animate-in-from-left'}`, className)}>
     {saved ? (
-        <IconStarFilled className={cn("w-6 h-6 text-yellow-400", className)} />
+        <IconStarFilled className={cn("w-6 h-6 text-yellow-400", iconClassName)} />
     ) : (
-        <IconStar className={cn("w-6 h-6 text-gray-200", className)} />
+        <IconStar className={cn("w-6 h-6 text-gray-200", iconClassName)} />
     )}
     </button>
   )
