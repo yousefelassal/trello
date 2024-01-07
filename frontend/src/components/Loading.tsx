59329668fb559d750d63271c3d05a72920a19699
Yousef <yousefsameh24@gmail.com>
2024-01-07 16:10:12 +0200

    Refactor Loading component to accept a className prop

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,6 +1,8 @@
-export default function Loading() {
+import { cn } from "@/lib/utils"
+
+export default function Loading({ className }: {className?:string}) {
   return (
-    <div className="h-full min-h-[70vh] w-full flex items-center justify-center">
+    <div className={cn("h-full w-full flex items-center justify-center", className)}>
         <div className="rounded-lg p-2 h-10 w-10 flex items-start justify-center gap-[2px] bg-[#9eacba]/80">
             <div className="h-[20px] animate-loading-reverse w-3 bg-[#1d2125]/80 rounded-[1px]" />
             <div className="h-[12px] animate-loading w-3 bg-[#1d2125]/80 rounded-[1px]" />
