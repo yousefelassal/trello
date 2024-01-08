ec261860989fd4eae9bdf116ac1818e555ae7432
Yousef <yousefsameh24@gmail.com>
2024-01-08 14:20:34 +0200

    Refactor Board component to use List component

--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -25,11 +25,12 @@ import {
     SaveMutationVariables,
     GetSavedDocument,
 } from "@/generated/graphql"
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { useDocumentTitle } from '@uidotdev/usehooks'
 import Star from "@/components/Star";
 import Loading from "@/components/Loading";
 import { Button } from "@/components/ui/button";
+import List from "@/components/List";
 
 export default function Board() {
   const { id } = useParams()
@@ -109,11 +110,11 @@ export default function Board() {
     setListTitle('')
   }
 
-  const addNewCard = async (list:any) => { //eslint-disable-line
+  const addNewCard = async (list:any, title:string) => { //eslint-disable-line
     addCard({
         variables: {
             listId: list.id,
-            title: '7aga gdeeda'
+            title: title
         },
         optimisticResponse: {
             __typename: "Mutation",
@@ -126,7 +127,7 @@ export default function Board() {
                     {
                         __typename: "Card",
                         id: `temp-${Date.now()}`,
-                        title: '7aga gdeeda',
+                        title: title,
                         description: ""
                     }
                 ]
@@ -325,55 +326,7 @@ export default function Board() {
               ref={provided.innerRef}
               className="container pt-16 flex gap-2">
             {data?.findBoard?.lists?.map((list, index) => (
-                <Draggable draggableId={list.id} index={index} key={list.id}>
-                {(provided:any) => ( //eslint-disable-line
-                <div 
-                  {...provided.draggableProps}
-                  ref={provided.innerRef}
-                  className="flex p-2 flex-col min-w-[272px] h-fit rounded-2xl shadow-2xl border border-[#201f1f] bg-[#101204] gap-2"
-                >
-                    <h3
-                        className="text-xl w-full font-bold"
-                        {...provided.dragHandleProps}
-                    >
-                        {list.title}
-                    </h3>
-                    <Droppable droppableId={list.id} type="task">
-                    {(provided:any, snapshot:any) => ( //eslint-disable-line
-                    <div 
-                      ref={provided.innerRef}
-                      {...provided.droppableProps}
-                      className={`flex flex-col rounded-lg gap-2 transition ${snapshot.isDraggingOver ? 'bg-[#595956]' : ''}`}
-                    >
-                        {list.cards?.map((card, index) => (
-                            <Draggable draggableId={card.id} index={index} key={card.id}>
-                            {(provided:any, snapshot:any) => ( //eslint-disable-line
-                                <div 
-                                  ref={provided.innerRef}
-                                  {...provided.draggableProps}
-                                  {...provided.dragHandleProps}
-                                  className={`${snapshot.isDragging ? 'bg-[#353f48] border-blue-500' : 'bg-[#22272B]' }
-                                  flex rounded-lg border-2 border-[#22272B] hover:border-blue-500 justify-center px-2 py-1 shadow-2xl flex-col`}
-                                >
-                                <h4 className="text-lg font-bold">{card.title}</h4>
-                                <div>{card.description}</div>
-                            </div>
-                            )}
-                            </Draggable>
-                        ))}
-                        {provided.placeholder}
-                        </div>
-                        )}
-                        </Droppable>
-                        <button
-                            onClick={() => addNewCard(list)}
-                            className="bg-[#ffffff3d] hover:bg-[#ffffff2a] transition rounded-lg h-fit p-2 flex items-center shadow-lg"
-                        >
-                            {list.cards?.length === 0 ? "Add a card" : "Add another card"}
-                        </button>
-                </div>
-                )}
-                </Draggable>
+                <List list={list} index={index} addNewCard={addNewCard} key={list.id} />
             ))}
             {provided.placeholder}
             {isAddingList ? 
