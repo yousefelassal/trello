521a9383848569045c6aba8555273d955e42acfb
Yousef <yousefsameh24@gmail.com>
2024-01-08 15:45:42 +0200

    Add delete board functionality and menu options

--- frontend/src/components/BoardHeader.tsx
+++ frontend/src/components/BoardHeader.tsx
@@ -4,10 +4,25 @@ import {
     SaveMutation,
     SaveMutationVariables,
     GetSavedDocument,
+    DeleteBoardDocument,
+    DeleteBoardMutation,
+    DeleteBoardMutationVariables
 } from "@/generated/graphql"
 import Star from "./Star"
+import { useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import { Trash, MoreHorizontal } from "lucide-react";
+import {
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
 
 export default function BoardHeader({board}:{board:any}) { //eslint-disable-line
+    const navigate = useNavigate()
+
     const [save] = useMutation<SaveMutation, SaveMutationVariables>(SaveDocument,{
         update: (cache, response) => {
           cache.updateQuery({ query: GetSavedDocument }, (data) => {
@@ -25,6 +40,13 @@ export default function BoardHeader({board}:{board:any}) { //eslint-disable-line
         })
       }
     });
+
+    const [deleteBoard] = useMutation<DeleteBoardMutation, DeleteBoardMutationVariables>(DeleteBoardDocument, {
+        update: (cache, response) => {
+          cache.evict({ id: 'Board:' + response.data?.deleteBoard?.id })
+          cache.gc()
+        }
+      })
 
     const handleSave = async () => {
         await save({
@@ -45,16 +67,46 @@ export default function BoardHeader({board}:{board:any}) { //eslint-disable-line
             }
           }
         })
-      }
+    }
+
+    const handleDelete = async () => {
+        await deleteBoard({
+          variables: {
+            id: board.id
+          }
+        })
+        navigate('/')
+    }
+
   return (
     <>
     <div className="-z-10 fixed inset-0 overflow-hidden">
         <img src={board.bg} alt="board background" className="object-cover absolute w-screen h-screen inset-0" />
     </div>
-    <div className="fixed px-6 backdrop-blur-sm py-2 w-screen shadow -mt-4 bg-black/60">
-        <div className="flex items-center gap-2">
-            <h2 className="text-2xl w-fit p-2 py-1 hover:bg-gray-700 rounded-lg font-bold">{board.title}</h2>
+    <div className="fixed px-6 backdrop-blur-sm pb-1 pt-3 w-screen shadow -mt-4 bg-black/60">
+        <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+                <h2 className="text-2xl w-fit p-2 py-1 hover:bg-gray-500/80 rounded-lg font-bold">{board.title}</h2>
                 <Star className="mt-1" saved={board.saved} onClick={handleSave} />
+            </div>
+            <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="ghost" className="px-2 rounded-lg hover:bg-gray-500/80">
+                    <MoreHorizontal />
+                </Button>
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Menu</SheetTitle>
+                </SheetHeader>
+                <div className="flex flex-col justify-between h-full py-6 w-full">
+                    <div>Change Background</div>
+                    <Button className="px-2 w-full flex gap-1" variant="destructive" onClick={handleDelete}>
+                        <Trash className="w-4 h-4" /> Delete Board
+                    </Button>
+                </div>
+            </SheetContent>
+            </Sheet>
         </div>
     </div>
     </>
