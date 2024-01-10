40bb3d24451f273a4e208a122802724ebb921aba
Yousef <yousefsameh24@gmail.com>
2024-01-10 15:32:36 +0200

    Update useRandomPhotos hook to handle background image selection

--- frontend/src/hooks/useRandomPhotos.tsx
+++ frontend/src/hooks/useRandomPhotos.tsx
@@ -5,13 +5,13 @@ const dataIfError = [
     {
         "id": "0z-sWoAqs0c",
         "urls": {
-            "raw": "https://images.unsplash.com/photo-1702288140268-608468d2d9eb?ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4ODYwOTJ8&ixlib=rb-4.0.3",
-            "full": "https://images.unsplash.com/photo-1702288140268-608468d2d9eb?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4ODYwOTJ8&ixlib=rb-4.0.3&q=85",
-            "regular": "https://images.unsplash.com/photo-1702288140268-608468d2d9eb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4ODYwOTJ8&ixlib=rb-4.0.3&q=80&w=1080",
-            "small": "https://images.unsplash.com/photo-1702288140268-608468d2d9eb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4ODYwOTJ8&ixlib=rb-4.0.3&q=80&w=400",
-            "thumb": "https://images.unsplash.com/photo-1702288140268-608468d2d9eb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4ODYwOTJ8&ixlib=rb-4.0.3&q=80&w=200",
-            "small_s3": "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1702288140268-608468d2d9eb"
-        }
+            "raw": "https://images.unsplash.com/photo-1704500355467-88351c0797c3?ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4OTE3NzV8&ixlib=rb-4.0.3",
+            "full": "https://images.unsplash.com/photo-1704500355467-88351c0797c3?crop=entropy&cs=srgb&fm=jpg&ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4OTE3NzV8&ixlib=rb-4.0.3&q=85",
+            "regular": "https://images.unsplash.com/photo-1704500355467-88351c0797c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4OTE3NzV8&ixlib=rb-4.0.3&q=80&w=1080",
+            "small": "https://images.unsplash.com/photo-1704500355467-88351c0797c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4OTE3NzV8&ixlib=rb-4.0.3&q=80&w=400",
+            "thumb": "https://images.unsplash.com/photo-1704500355467-88351c0797c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1NDc0MTJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MDQ4OTE3NzV8&ixlib=rb-4.0.3&q=80&w=200",
+            "small_s3": "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1704500355467-88351c0797c3"
+        },
     },
     {
         "id": "01jCnEZBPIc",
@@ -52,6 +52,11 @@ const useRandomPhotos = () => {
     const [data, setData] = useState<any>([])// eslint-disable-line
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
+    const [selectedBg, setSelectedBg] = useState('')
+
+    const handleBgChange = (bg: string) => {
+        setSelectedBg(bg)
+    }
 
     useEffect(() => {
         setIsLoading(true)
@@ -60,6 +65,7 @@ const useRandomPhotos = () => {
             .then((res) => res.json())
             .then((data) => {
                 setData(data)
+                setSelectedBg(data[0].urls.regular)
                 setIsLoading(false)
             })
             .catch((error) => {
@@ -72,7 +78,9 @@ const useRandomPhotos = () => {
     return {
         photos: data.map((photo:any) => photo.urls.regular), // eslint-disable-line
         error,
-        isLoading
+        isLoading,
+        selectedBg,
+        handleBgChange
     }
 }
 
