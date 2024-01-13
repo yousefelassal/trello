789f5f8e989f4e575b799aaeff84ca5e6030f0e6
Yousef <yousefsameh24@gmail.com>
2024-01-13 15:09:52 +0200

    Update Docs page styling

--- frontend/src/pages/Docs.tsx
+++ frontend/src/pages/Docs.tsx
@@ -6,8 +6,8 @@ export default function Docs() {
   useDocumentTitle('Docs | Trello 3al daya2')
   return (
     <div className="min-h-screen w-full rounded-md flex items-center justify-center antialiased bg-grid-white/[0.02] relative overflow-hidden">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-        <div className="max-w-3xl w-full px-4 py-8 sm:px-8 sm:py-12 mx-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black/40 md:bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        <div className="max-w-3xl w-full px-4 py-16 sm:px-8 mx-0">
           <Content />
         </div>
     </div>
