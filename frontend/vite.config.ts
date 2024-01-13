c6c9b33f4734c7f1fc48ea3aace6b67f62f155b8
Yousef <yousefsameh24@gmail.com>
2024-01-13 14:46:22 +0200

    Add remarkCodeHike plugin to Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,25 @@
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { remarkCodeHike } from '@code-hike/mdx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(async () => {
+  const mdx = await import('@mdx-js/rollup')
+  return {
+    optimizeDeps: {
+      include: ['react/jsx-runtime'],
     },
-  },
+    plugins: [
+      mdx.default({
+        remarkPlugins: [[remarkCodeHike, { theme: 'github-dark' }]],
+      }),
+      react()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
 })
