414cc48667c2202d3e76534afd3e8b8406056679
Yousef <yousefsameh24@gmail.com>
2024-01-14 15:10:45 +0200

    Add open graph image field to attachment model

--- backend/models/attachment.js
+++ backend/models/attachment.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const schema = new mongoose.Schema({
     url: String,
     name: String,
-    uploaded_at: { type: Date, default: Date.now }
+    uploaded_at: { type: Date, default: Date.now },
+    open_graph_image: String,
 })
 
 schema.set("toJSON", {
